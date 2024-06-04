e7782ea4cdb0298913044e44f6e25ed6c1d44ef6
Alberto <ahernandezlopez@safareyes.es>
2024-06-04 19:53:07 +0200

    Creacion de varios htmls, nuevo urls.py, creacion de static con imagenes nuevas

--- AgendaContactosApp/urls.py
+++ AgendaContactosApp/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import *
 
-from AgendaContactosApp.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ('AgendaContactos/', include('AgendaContactosApp.urls')),
-    path('', inicio, name='inicio'),
+    path('inicio/', inicio, name='inicio'),
+    path('usuario/', usuario, name='usuario'),
+    path('sobre_nosotros/', sobre_nosotros, name='sobre_nosotros'),
+    path('contactos/', contactos, name='contactos'),
+
 
 ]
--- AgendaContactosApp/views.py
+++ AgendaContactosApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from .models import *
+
+
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def usuario(request):
+    return render(request, 'usuario.html')
+
+def sobre_nosotros(request):
+    return render(request, 'sobre_nosotros.html')
+
+def contactos(request):
+    lista_contactos = Contactos.objects.all()
+    return render(request, 'contactos.html', {'lista_contactos': lista_contactos})
+
